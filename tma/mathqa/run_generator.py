05a04d36b9d38c3b300917a5bba2c83d3f27a385
toberjia <toberjia@uw.edu>
2024-07-18 21:10:49 -0700

    6 more generator

--- tma/mathqa/run_generator.py
+++ tma/mathqa/run_generator.py
@@ -1,5 +1,5 @@
 import random
-from geometry_task import MathTemplateMetaData, PerimeterGenerator, MidpointGenerator, IntersectionGenerator, CircleGenerator, AngleGenerator, TaskStore
+from geometry_task import MathTemplateMetaData, PerimeterGenerator, MidpointGenerator, IntersectionGenerator, CircleGenerator, AngleGenerator, PerpendicularGenerator, SideLengthGenerator, ArcLengthGenerator,TaskStore
 
 def main():
     # Define the path to templates using a raw string
@@ -8,24 +8,27 @@ def main():
     Perimeter_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\perimeter_templates.json'
     Midpoint_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\midpoint_templates.json'
     Intersection_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\intersection_templates.json'
+    Perpendicular_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\perpendicular_template.json'
+    SideLength_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\sideLength_template.json'
+    ArcLength_template_path = r'C:\Users\jiata\OneDrive\Desktop\Raivn\TaskMeAnyMath\annotations\math_annotations\arcLength_template.json'
 
-    Circle_metadata = MathTemplateMetaData(template_path=Circle_template_path)
+    ArcLength_metadata = MathTemplateMetaData(template_path=ArcLength_template_path)
 
-    Circle_task_store = TaskStore(schema = CircleGenerator.schema)
+    ArcLength_task_store = TaskStore(schema = ArcLengthGenerator.schema)
 
-    # metadata = MathTemplateMetaData(template_path=Circle_template_path)
-    metadata = MathTemplateMetaData(template_path=Circle_template_path)
+    # metadata = MathTemplateMetaData(template_path=ArcLength_template_path)
+    metadata = MathTemplateMetaData(template_path=ArcLength_template_path)
 
-    Circle_generator = CircleGenerator(metadata=Circle_metadata)
-    Circle_generator.enumerate_task_plans(Circle_task_store)
+    ArcLength_generator = ArcLengthGenerator(metadata=ArcLength_metadata)
+    ArcLength_generator.enumerate_task_plans(ArcLength_task_store)
  
     # Select and print a random task
-    Circle_tasks = list(Circle_task_store)
-    if Circle_tasks:
-        random_Circle_task = random.choice(Circle_tasks)
-        Circle_task = Circle_generator.generate(random_Circle_task)
-        print("Random Circle Task:")
-        print("Question:", Circle_task['question'])
-        print("Answer:", Circle_task['answer'])
+    ArcLength_tasks = list(ArcLength_task_store)
+    if ArcLength_tasks:
+        random_ArcLength_task = random.choice(ArcLength_tasks)
+        ArcLength_task = ArcLength_generator.generate(random_ArcLength_task)
+        print("Random ArcLength Task:")
+        print("Question:", ArcLength_task['question'])
+        print("Answer:", ArcLength_task['answer'])
 if __name__ == "__main__":
     main()
